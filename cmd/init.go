917a8b133838bb56e0627869b5b1bafd267c9b90
WoutVossen <vossen.w@hotmail.com>
2024-09-22 15:46:34 +0200

    Add loading api key through env var

--- cmd/init.go
+++ cmd/init.go
@@ -11,8 +11,9 @@ import (
 
 // Define a default template configuration
 var defaultConfig = []byte(`# Configuration for the crev tool
-# specify your Code AI Review API key (necessary for review command)
-api-key: # Fill in your CREV API key here
+
+# specify your CREV API key (necessary for review command) ! this overwrites the value you specify in the environment variable
+crev_api_key: # ex. csk_8e796a8f6fdb15f0902eee0d4138b9d5975e244e6cc61ef302feaf37af24c7cb
 # specify the prefixes of files and directories to ignore (by default common configuration files are ignored)
 ignore-pre: # ex. [tests, readme.md, scripts]
 # specify the extensions of files to ignore 
